a556dc3e5fbebf7445bc6f906735291d8656fd0d
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-11-08 19:18:44 +0600

    data generate try

--- GAN_dtc.py
+++ GAN_dtc.py
@@ -3,7 +3,31 @@ import torchvision.transforms as transforms
 import numpy as np
 from data.utils import TransformTwice, TransformKtimes, RandomTranslateWithReflect, TwoStreamBatchSampler
 from torch.utils.data import DataLoader, TensorDataset
-from GAN_denoise import Generator
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 3, 1, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.gen(x)
 
 
 # Define constants for CIFAR-10 normalization
